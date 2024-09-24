34cad086c77d9d1807fd93425b6b8bae4f55217a
daniellasilva.design@gmail.com <daniellasilva.design@gmail.com>
2024-09-24 13:13:56 -0600

    first commit

---
+++ Day 1/main.js
@@ -0,0 +1,24 @@
+let numeroUno = 1
+let stringUno = '1'
+let numeroTreinta = 30
+let stringTreinta = '30'
+let numeroDiez = 10
+let stringDiez = '10'
+
+if (numeroUno === stringUno) {
+    console.log('Las variables numeroUno y stringUno tienen el mismo valor, pero diferentes tipos');
+} else {
+  console.log('Como variables numeroUno y stringUno no tienen el mismo valor');
+}
+
+if (numeroTreinta === stringTreinta) {
+    console.log('Las variables numeroTreinta y stringTreinta tienen el mismo valor, pero diferentes tipos');
+} else {
+  console.log('Como variables numeroTreinta y stringTreinta no tienen el mismo valor');
+}
+
+if (numeroDiez === stringDiez) {
+    console.log('Las variables numeroDiez y stringDiez tienen el mismo valor, pero diferentes tipos');
+} else {
+  console.log('Como variables numeroDiez y stringDiez no tienen el mismo valor');
+}
